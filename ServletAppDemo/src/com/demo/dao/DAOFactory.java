f00e4c51c4a701256a070097a141a01e469538ff
orlando <orlandolozaarr@gmail.com>
2018-07-21 09:49:25 -0500

    master

--- ServletAppDemo/src/com/demo/dao/DAOFactory.java
+++
@@ -1,7 +0,0 @@
-package com.demo.dao;
-
-public class DAOFactory {
-	public static MySqlDB getMySqlDB(){
-		return new MySqlDB();
-	}
-}
--- ServletAppDemo/src/com/demo/dao/StateDAO.java
+++
@@ -1,9 +0,0 @@
-package com.demo.dao;
-
-import java.util.List;
-
-import com.demo.model.State;
-
-public interface StateDAO {
-	public List<State> getAllStates();
-}
